2a5b586b11fd7581ecdb2bc732844b1de78ab91a
Kamlesh Singh Bisht <kamlesh.bisht@infosys.com>
2025-02-11 11:03:52 +0530

    [INJIWEB-1149]: Fix Scroll & Overlap
    Signed-off-by: Kamlesh Singh Bisht <kamlesh.bisht@infosys.com>

--- inji-web/src/modals/ModalWrapper.tsx
+++ inji-web/src/modals/ModalWrapper.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import {ModalWrapperProps} from "../types/components";
 export const ModalWrapper: React.FC<ModalWrapperProps> = (props) => {
+
+    // Effect to lock/unlock scroll on body when modal is open/closed
+    useEffect(() => {
+      // Lock scrolling when the modal is open
+      document.body.style.overflow = 'hidden';
+  
+      // Cleanup: Unlock scrolling when the modal is closed
+      return () => {
+        document.body.style.overflow = '';
+      };
+    }, []); // Empty dependency array ensures this runs on mount/unmount only
+
   return <>
-    <div data-testid="ModalWrapper-Outer-Container" className={`fixed inset-0 ${props.zIndex == 50 ? 'z-50' : 'z-40'} overflow-y-auto overflow-x-hidden`}>
+   <div data-testid="ModalWrapper-Outer-Container" className={`fixed inset-0 ${props.zIndex == 50 ? 'z-50' : 'z-40'} overflow-y-auto overflow-x-hidden`}>
       <div className="min-h-full p-0 flex items-center justify-center">
         <div className={`w-auto my-8 mx-4 sm:mx-6 ${props.size === '3xl' ? 'max-w-3xl' : 'max-w-sm'} `}>
           <div data-testid="ModalWrapper-Inner-Container" className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-iw-background outline-none focus:outline-none">
