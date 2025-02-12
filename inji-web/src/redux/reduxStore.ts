ed58837a3d1f41907368ab05cc4ae0789037fb56
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2025-02-12 18:15:37 +0530

    [INJIWEB-1429] use session storage to store issuers list instead of local storage as we don't want to persist it even if the browser is closed
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>

--- inji-web/src/redux/reduxStore.ts
+++ inji-web/src/redux/reduxStore.ts
@@ -1,14 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit";
 import {persistReducer, persistStore, createTransform} from "redux-persist";
-import storage from "redux-persist/lib/storage"; // Use AsyncStorage for React Native
+import storageSession from "redux-persist/lib/storage/session";
 import {issuersReducer} from "./reducers/issuersReducer";
 import {credentialsReducer} from "./reducers/credentialsReducer";
 import {commonReducer} from "./reducers/commonReducer";
 
 const issuersPersistConfig = {
     key: "issuers",
-    storage,
-    whitelist: ["issuers"], //  mention issuers reducer field name (issuers) that needs to be persisted
+    storage: storageSession,
+    whitelist: ["issuers"] //  mention issuers reducer field name (issuers) that needs to be persisted
 };
 
 const persistedIssuersReducer = persistReducer(
