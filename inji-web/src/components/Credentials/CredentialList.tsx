53e2b1a9bffad77b928cb9247df3d76b4e0cf853
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2025-02-14 20:34:11 +0530

    [INJIWEB-1429] show the credential types of an issuer only if issuer wellknown has language support for selected or default language
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>

--- inji-web/src/components/Credentials/CredentialList.tsx
+++ inji-web/src/components/Credentials/CredentialList.tsx
@@ -9,15 +9,54 @@ import {SpinningLoader} from "../Common/SpinningLoader";
 import {CredentialListProps} from "../../types/components";
 import {HeaderTile} from "../Common/HeaderTile";
 import {DownloadResult} from "../Redirection/DownloadResult";
-import {IssuerObject} from "../../types/data";
+import {CredentialConfigurationObject} from "../../types/data";
+import {defaultLanguage} from "../../utils/i18n";
 
 export const CredentialList: React.FC<CredentialListProps> = ({state}) => {
     const [errorObj, setErrorObj] = useState({
         code: "",
         message: ""
     });
-    
+    const selectedLanguage = useSelector(
+        (state: RootState) => state.common.language
+    );
+
     const credentials = useSelector((state: RootState) => state.credentials);
+
+    const filterCredentialsBySelectedOrDefaultLanguage = () => {
+        const missingLanguageSupport: string[] = [];
+
+        const filteredCredentialsList = Object.entries(
+            credentials?.filtered_credentials
+                ?.credential_configurations_supported || {}
+        ).filter(([credentialType, credential]) => {
+            const {display, credential_definition} =
+                credential as CredentialConfigurationObject; // Destructure
+            const hasMatchingDisplay = display?.some(
+                ({locale}) =>
+                    locale === selectedLanguage || locale === defaultLanguage
+            );
+
+            if (!hasMatchingDisplay) {
+                missingLanguageSupport.push(credential_definition.type[1]);
+            }
+            
+            return hasMatchingDisplay;
+        });
+
+        if (missingLanguageSupport.length) {
+            console.error(
+                `Language support missing for these credential types of issuer: ${missingLanguageSupport.join(
+                    ", "
+                )}`
+            );
+        }
+        return Object.fromEntries(filteredCredentialsList);
+    };
+
+    const filteredCredentialsWithLangSupport =
+        filterCredentialsBySelectedOrDefaultLanguage();
+
     const {t} = useTranslation("CredentialsPage");
 
     if (state === RequestStatus.LOADING) {
@@ -26,6 +65,7 @@ export const CredentialList: React.FC<CredentialListProps> = ({state}) => {
 
     if (
         state === RequestStatus.ERROR ||
+        Object.keys(filteredCredentialsWithLangSupport).length == 0 ||
         !credentials?.filtered_credentials
             ?.credential_configurations_supported ||
         (credentials?.filtered_credentials
@@ -61,21 +101,22 @@ export const CredentialList: React.FC<CredentialListProps> = ({state}) => {
                 subContent={t("containerSubHeading")}
             />
             <div className="flex flex-wrap gap-3 p-4 pb-20 justify-start">
-                {credentials?.filtered_credentials &&
-                    Object.keys(
-                        credentials?.filtered_credentials
-                            ?.credential_configurations_supported
-                    ).map((credentialId: string, index: number) => (
+                {Object.keys(filteredCredentialsWithLangSupport).map(
+                    (credentialId: string, index: number) => (
                         <Credential
                             credentialId={credentialId}
                             credentialWellknown={
                                 credentials?.filtered_credentials
+                                    ?.credential_configurations_supported[
+                                    credentialId
+                                ]
                             }
                             key={index}
                             index={index}
                             setErrorObj={setErrorObj}
                         />
-                    ))}
+                    )
+                )}
             </div>
         </>
     );
